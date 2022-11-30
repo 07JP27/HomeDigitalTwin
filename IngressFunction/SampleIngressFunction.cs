b1863a930225c4915103d800661768bb4690150e
07JP27 <jynpei.t@gmail.com>
2022-11-30 19:56:59 +0900

    Add ingress function

---
+++ IngressFunction/SampleIngressFunction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Azure;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace HomeDigitalTwinIngressFunction
+{
+    public static class SampleIngressFunction
+    {
+        private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
+        private static readonly HttpClient httpClient = HttpClientFactory.Create();
+
+        [FunctionName("SampleIngress")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            try
+            {
+                if (adtInstanceUrl == null) throw new ArgumentNullException("adtInstanceUrl");
+
+                // Authenticate with Digital Twins
+                var cred = new DefaultAzureCredential();
+                var client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
+                log.LogInformation($"ADT service client connection created.");
+               
+                // <Update_twin_with_device_temperature>
+                var updateTwinData = new Azure.JsonPatchDocument();
+                updateTwinData.AppendReplace("/Temperature", 12.5);
+                updateTwinData.AppendReplace("/Humidity", 99.3);
+                await client.UpdateDigitalTwinAsync("Meter1", updateTwinData);
+                // </Update_twin_with_device_temperature>
+            }
+            catch (Exception ex)
+            {
+                log.LogError($"Error in ingest function: {ex.Message}");
+                return new BadRequestObjectResult(ex.Message);
+            }
+
+            return new OkResult();
+        }
+    }
+}
+
